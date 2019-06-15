f628648604166bc673d8d75a7ce6848dc1076a89
Nicoowz <51835658+Nicoowz@users.noreply.github.com>
2019-06-15 14:19:12 -0400

    Update gamecard.php

--- gamecard.php
+++ gamecard.php
@@ -3,9 +3,38 @@
     <div class="bgg-data">
         <h1><a href="https://boardgamegeek.com/boardgame/<?php echo $gameid?>/"><?php echo $bg->name[0]?></a> <span>(<?php echo $bg->yearpublished?>)</span></h1>
         <ul class="bgg-details">
-            <li><?php echo $bg->minplayers." - ".$bg->maxplayers?> Players</li>
-            <li><?php echo $bg->minplaytime." - ".$bg->maxplaytime?> Min</li>
+            <li>
+				<?php
+/* Número Jugadores: */
+if (2 > $bg->maxplayers):
+    echo $bg->minplayers;
+else:
+    echo $bg->minplayers." - ".$bg->maxplayers;
+endif;
+?> 
+<?php
+/* Jugador/es: */
+if (2 > $bg->maxplayers):
+    echo "Player";
+else:
+    echo "Players";
+endif;
+?>
+			</li>
+            <li>
+				<?php
+$a = (string) $bg->minplaytime;
+
+/* Duración */
+if ( $a == $bg->maxplaytime): 
+    echo $bg->minplaytime;
+else:
+    echo $bg->minplaytime." - ".$bg->maxplaytime;
+endif;
+?>
+ Min</li>
             <li>Age: <?php echo $bg->age?>+</li>
+			
         </ul>
     </div>
 </div>
